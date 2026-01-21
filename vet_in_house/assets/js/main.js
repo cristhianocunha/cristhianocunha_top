d85a5e5547c1e1fcbd7664f570b2ba33dd5e80a5
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2026-01-21 09:29:59 -0300

    add  form n8n

--- vet_in_house/assets/js/main.js
+++ vet_in_house/assets/js/main.js
@@ -122,3 +122,26 @@
 
 })(jQuery);
 
+const modal = document.getElementById("n8nModal");
+        const btn = document.getElementById("openModal");
+        const span = document.getElementsByClassName("close")[0];
+
+        // Abrir o modal
+        btn.onclick = function() {
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden"; // Impede scroll no fundo
+        }
+
+        // Fechar o modal no 'X'
+        span.onclick = function() {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto";
+        }
+
+        // Fechar se clicar fora do modal
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                document.body.style.overflow = "auto";
+            }
+        }
